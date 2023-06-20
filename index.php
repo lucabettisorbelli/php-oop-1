052ba4c357c56a217eebf8a87f837227f28fd1e7
luca betti sorbelli <luca betti sorbelli>
2023-06-20 17:30:10 +0200

    array di generi

--- index.php
+++ index.php
@@ -1,26 +1,54 @@
 <?php
 
+class Genre
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
+
 class Movie
 {
     public $title;
     public $director;
     public $year;
+    public $genres;
 
-    public function __construct($title, $director, $year)
+    public function __construct($title, $director, $year, $genres)
     {
         $this->title = $title;
         $this->director = $director;
         $this->year = $year;
+        $this->genres = array();
+
+        foreach ($genres as $genre) {
+            if ($genre instanceof Genre) {
+                array_push($this->genres, $genre);
+            }
+        }
     }
 
     public function getInfo()
     {
-        return "Titolo: " . $this->title . ", Regista: " . $this->director . ", Anno: " . $this->year;
+        $genres = array();
+        foreach ($this->genres as $genre) {
+            array_push($genres, $genre->name);
+        }
+
+        return "Titolo: " . $this->title . ", Regista: " . $this->director . ", Anno: " . $this->year . ", Generi: " . implode(", ", $genres);
     }
 }
 
-$movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972);
-$movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994);
+$genre1 = new Genre("Drammatico");
+$genre2 = new Genre("Gangster");
+$genre3 = new Genre("Thriller");
+$genre4 = new Genre("Noir");
+
+$movie1 = new Movie("Il Padrino", "Francis Ford Coppola", 1972, [$genre1, $genre2]);
+$movie2 = new Movie("Pulp Fiction", "Quentin Tarantino", 1994, [$genre3, $genre4]);
 
 echo $movie1->getInfo() . '<br>';
 echo $movie2->getInfo();
